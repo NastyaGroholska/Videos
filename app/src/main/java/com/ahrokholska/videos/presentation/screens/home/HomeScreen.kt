8406c8c90ebe6f7d74367749a7815cd87d431449
Anastasiia <anastasiiahrokholska@gmail.com>
2024-09-25 11:45:03 +0300

    Added credit to Pexels

--- app/src/main/java/com/ahrokholska/videos/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/ahrokholska/videos/presentation/screens/home/HomeScreen.kt
@@ -15,16 +15,20 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -41,9 +45,21 @@ fun HomeScreen(onVideoClick: (Int) -> Unit, viewModel: HomeViewModel = hiltViewM
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenContent(videos: List<VideoUI>?, onVideoClick: (Int) -> Unit = {}) {
-    Scaffold { innerPadding ->
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                val uriHandler = LocalUriHandler.current
+                Text(
+                    modifier = Modifier.clickable { uriHandler.openUri("https://www.pexels.com") },
+                    text = stringResource(R.string.videos_provided_by_pexels),
+                    textDecoration = TextDecoration.Underline
+                )
+            })
+        }
+    ) { innerPadding ->
         if (videos == null) {
             Box(
                 modifier = Modifier
